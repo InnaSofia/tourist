0c6275c05e12036243d0d3a6fce26283121867d7
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-10-12 01:08:44 +0300

    документация

--- src/app.js
+++ src/app.js
@@ -1,43 +1,41 @@
-import { format, differenceInDays } from 'date-fns';
-import { ru } from 'date-fns/locale';
+import { format, differenceInDays } from "date-fns"
+import { ru } from "date-fns/locale"
 
 let tours = []
 
 async function loadTours() {
-  const response = await fetch(
-    'https://www.bit-by-bit.ru/api/student-projects/tours'
-  );
-  const data = await response.json()
-  return data
+    const response = await fetch(
+        "https://www.bit-by-bit.ru/api/student-projects/tours"
+    )
+    const data = await response.json()
+    return data
 }
 
 async function init() {
-  const tours = await loadTours()
-  renderTours(tours)
-  
+    const tours = await loadTours()
+    renderTours(tours)
 }
 init()
 
 function renderTours(tours) {
-
-    let containerTours = document.getElementById('containerTours')
-    containerTours.innerHTML = '';
-  tours.forEach((tour) => {
-    const duration = differenceInDays(
-      new Date(tour.endTime),
-      new Date(tour.startTime)
-    );
-    containerTours.innerHTML += `
+    let containerTours = document.getElementById("containerTours")
+    containerTours.innerHTML = ""
+    tours.forEach((tour) => {
+        const duration = differenceInDays(
+            new Date(tour.endTime),
+            new Date(tour.startTime)
+        )
+        containerTours.innerHTML += `
    
        <div id="tourID" class="bg-white shadow-lg rounded-lg overflow-hidden p-2 flex flex-col justify-between duration-{amount} transition duration-150 ease-in-out transform hover:scale-105">
 
 
        
        <div><div class="bg-indigo-800 text-white w-8 text-center rounded-full absolute m-2">${
-         tour.rating
+           tour.rating
        }</div>
        <img class="rounded h-32 w-full sm:h-32 md:h-40" src="${
-         tour.image
+           tour.image
        }"></div>
 
        <div class="flex flex-col">
@@ -47,12 +45,12 @@ function renderTours(tours) {
 
        <span class="text-xs text-slate-500 pt-3">${tour.country}</span>
 ${
-  tour.city !== null
-    ? `  <span class="text-slate-500 pt-6 px-1">&middot;
+    tour.city !== null
+        ? `  <span class="text-slate-500 pt-6 px-1">&middot;
     </span>  
     <span class="text-xs text-slate-500 pt-3">${tour.city}</span>
     `
-    : ''
+        : ""
 }
 
 
@@ -61,7 +59,7 @@ ${
     
 
        <div class="text-lg text-indigo-800 pt-3 font-semibold leading-normal">${
-         tour.hotelName
+           tour.hotelName
        }</div>
 
 
@@ -69,9 +67,9 @@ ${
 
 
        <div class="text-xs pt-3 text-slate-500">${format(
-         new Date(tour.startTime),
-         `dd MMMM yyyy`,
-         { locale: ru }
+           new Date(tour.startTime),
+           `dd MMMM yyyy`,
+           { locale: ru }
        )} - ${format(new Date(tour.endTime), `dd MMMM yyyy`, { locale: ru })} 
     Продплжительность: ${duration}</div>
     </div>
@@ -79,7 +77,9 @@ ${
     <div>
 
     <div class="pt-3 flex justify-between">
-    <button id="btnReservation-${tour.id}" class="bg-white border-2 rounded-lg border-indigo-700 text-xs text-indigo-800 pt-1 leading-normal p-1.5 hover:bg-indigo-800 hover:text-white w-28">Забронировать</button>
+    <button id="btnReservation-${
+        tour.id
+    }" class="bg-white border-2 rounded-lg border-indigo-700 text-xs text-indigo-800 pt-1 leading-normal p-1.5 hover:bg-indigo-800 hover:text-white w-28">Забронировать</button>
     <button class="bg-blue-800 border-2 rounded-lg border-indigo-800 text-xs text-white pt-1 leading-normal p-1.5 hover:bg-white hover:text-indigo-800 w-28">В избранное</button>
     </div>
     </div>
@@ -91,48 +91,49 @@ ${
     
     
     
-    `;
+    `
 
-    
-    tours.forEach((tour) =>{
-        const btnBook = document.getElementById(`btnReservation-${tour.id}`) 
-      btnBook.addEventListener('click', () =>{
-        openWindow(tour.id)  
-      });
+        tours.forEach((tour) => {
+            const btnBook = document.getElementById(`btnReservation-${tour.id}`)
+            btnBook.addEventListener("click", () => {
+                openWindow(tour.id)
+            })
+        })
     })
-  })}
+}
 
-const modalWindow = document.getElementById('modalWindow'); //окно
-const сloseWindows = document.getElementById('BtnCloseWindows');//кнопка закрыть
-const btnReservation = document.getElementById(`btnReservation-${tours.id}`); //кнопка забронировать т.е. открыть окно
-const bookTourBtn = document.getElementById('btnToSend')
+const modalWindow = document.getElementById("modalWindow") //окно
+const сloseWindows = document.getElementById("BtnCloseWindows") //кнопка закрыть
+const btnReservation = document.getElementById(`btnReservation-${tours.id}`) //кнопка забронировать т.е. открыть окно
+const bookTourBtn = document.getElementById("btnToSend")
 
-bookTourBtn.addEventListener('click',(event) => bookTour(event))
+bookTourBtn.addEventListener("click", (event) => bookTour(event))
 //открываем
-let currentId;
+let currentId
 
 function openWindow(id) {
-  currentId = id;
+    currentId = id
 
-  modalWindow.style.display = 'flex';
+    modalWindow.style.display = "flex"
 
-  tours.find((u) => {return u.id === id;});
-  сloseWindows.addEventListener('click', сloseWindows)
- 
+    tours.find((u) => {
+        return u.id === id
+    })
+    сloseWindows.addEventListener("click", сloseWindows)
 }
 
-function сloseWindows(){
-    modalWindow.style.display='none'
+function сloseWindows() {
+    modalWindow.style.display = "none"
 }
 
-async function bookTour(t){
-    const form = document.getElementById('form')
+async function bookTour(t) {
+    const form = document.getElementById("form")
 
-    const userName = document.getElementById('name').value
-    const userPhone = document.getElementById('phone').value
-    const userEmail = document.getElementById('email').value
-    const userComment = document.getElementById('comment').value
-    
+    const userName = document.getElementById("name").value
+    const userPhone = document.getElementById("phone").value
+    const userEmail = document.getElementById("email").value
+    const userComment = document.getElementById("comment").value
+
     let userData = {
         name: userName,
         phone: userPhone,
@@ -143,29 +144,25 @@ async function bookTour(t){
     const tourID = currentId
 
     const url = `https://www.bit-by-bit.ru/api/student-projects/tours/${tourID}`
-      
-    let response = await fetch (url, {
-        method: "POST", 
+
+    let response = await fetch(url, {
+        method: "POST",
         body: JSON.stringify(userData)
-      })
-      if(response.ok){
+    })
+    if (response.ok) {
         alert("Ваше обращение зарегистрировано")
         сloseWindows()
-        let result = await response.json() 
+        let result = await response.json()
         return result
-      }else{
+    } else {
         alert("Повторите ещё раз! ПРОИЗОШЛА ОШИБКА")
-      }
+    }
 }
 
 //очищение полей
 function clearWindow() {
-  document.getElementById('name').value = '';
-  document.getElementById('phone').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('comment').value = '';
+    document.getElementById("name").value = ""
+    document.getElementById("phone").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("comment").value = ""
 }
-
-
-
-
